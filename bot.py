31cb4fc84edae5884824d0883fe8bb92e3dbd622
lizaost <lizaost@gmail.com>
2020-09-30 18:09:14 +0600

    Add generated password length and method selection with placeholder variants

--- bot.py
+++ bot.py
@@ -9,12 +9,14 @@ class PasswordsBot:
         if message.text == '/check':
             return self.check_password_strength_welcome()
         if message.text == '/generate':
-            return self.password_generation_variants()
+            return self.password_generation_length()  # length -> methods -> results
         if message.text == '/recommendations':
             return self.recommendations()
         return BotAnswer('I don\'t understand you')
 
     def get_button_action_response(self, action, data):
+        if action == 'generate_length':
+            return self.password_generation_methods(data)
         if action == 'generate':
             return self.password_generation_results(data)
         return BotAnswer('I don\'t understand you')
@@ -25,15 +27,31 @@ class PasswordsBot:
     def recommendations(self):
         return BotAnswer('InfoSec Recommendations')
 
-    def password_generation_variants(self):
+    def password_generation_methods(self, length):
         # second parameter of bot answer should be a keyboard with possible generation variants
         # example - ({'text': 'RED', 'data': 'red button'}, {'text': 'BLUE', 'data': 'blue button'})
-        # in data a string "generate <GENERATION METHOD>" should be passed ("generate abc")
+        # in data a string "generate_method <LENGTH> <METHOD>" should be passed ("generate 8 abc")
         # it will be checked in the bot api to route to correct function (password generation)
-        return BotAnswer('password generation variants with keyboard')
+        #    generate 8 abc
+        print("Length from previous step is " + length)
+        return BotAnswer('available password generation methods variants with keyboard',
+                         ({'text': 'symbols', 'data': f'generate {length} abc'},
+                          {'text': 'numbers', 'data': f'generate {length} 123'}))
 
-    def password_generation_results(self, method):
-        return BotAnswer(f'list of generated passwords using method {method}')
+    def password_generation_length(self):
+        # second parameter of bot answer should be a keyboard with possible generation variants
+        # example - ({'text': 'RED', 'data': 'red button'}, {'text': 'BLUE', 'data': 'blue button'})
+        # in data a string "generate_length <LENGTH>" should be passed ("generate_length 8")
+        # it will be checked in the bot api to route to correct function (password generation)
+        # 8 9 10 12 14 16 18 20
+        # generate_length 8
+        print()
+        return BotAnswer('possible password generation lengths with keyboard',
+                         ({'text': '8', 'data': 'generate_length 8'}, {'text': '9', 'data': 'generate_length 9'}))
+
+    def password_generation_results(self, data):
+        [length, method] = data.split(" ")
+        return BotAnswer(f'list of generated passwords of length {length} using method {method}')
 
     def check_password_strength_welcome(self):
         return BotAnswer('a request for user to enter a password to be checked')
